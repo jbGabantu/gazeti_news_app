25ca642636dc642e849b3187d4836d2fd1a81075
jbGabantu <gabantuj@gmail.com>
2020-07-27 16:11:43 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gazeti-news-app
+
+Gazeti (Swahili for newspaper) is a web application that allows a user to access top news headlines.
---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/normalize.css";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
